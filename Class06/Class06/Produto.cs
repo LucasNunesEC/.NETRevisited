6073158334d82565046182185e219201f2785fa0
Unknown <lucas.nunes@eng.ci.ufpb.br>
2018-08-13 11:20:32 -0300

    Classes - Registro de Produtos

---
+++ Class06/Class06/Produto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace Class06
+{
+    class Produto
+    {
+        public string nome;
+        public double preco;
+        public int quantidadeEmEstoque;
+
+        public double ValorTotalEmEstoque() {
+            return preco * quantidadeEmEstoque;
+        }
+
+        public void RealizarEntrada(int quantidade) {
+            quantidadeEmEstoque += quantidade;
+        }
+
+        public void RealizarSaida(int quantidade) {
+            quantidadeEmEstoque -= quantidade;
+        }
+
+
+        public override string ToString() {
+            return nome
+                + ", R$"
+                + preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + quantidadeEmEstoque
+                + " unidades, Total:  R$: "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+    }
+}
---
+++ Class06/Class06/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Class06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtde;
+            Produto p;
+            p = new Produto();
+
+            Console.WriteLine("Digite os dados do produto : ");
+            Console.Write("Nome: ");
+            p.nome = Console.ReadLine();
+
+            Console.Write("Preco: ");
+            p.preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Qtde em Estoque: ");
+            p.quantidadeEmEstoque = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do Produto: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite a qtde de dprodutos que entraram: ");
+            qtde = int.Parse(Console.ReadLine());
+            p.RealizarEntrada(qtde);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite a qtde de dprodutos que sairam: ");
+            qtde = int.Parse(Console.ReadLine());
+            p.RealizarSaida(qtde);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: " + p);
+
+            Console.ReadLine();
+        }
+    }
+}
