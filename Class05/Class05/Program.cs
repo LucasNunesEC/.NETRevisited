cdfb81f358ec92a914c59b07c18b73ccdb6c8a57
Unknown <lucas.nunes@eng.ci.ufpb.br>
2018-08-13 10:51:46 -0300

    Classes - Calculo de area de Triangulo

---
+++ Class05/Class05/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Class05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo X, Y;
+            double areaX, areaY;
+
+            X = new Triangulo();
+            Y = new Triangulo();
+
+            X.ladoA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            X.ladoB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            X.ladoC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Y.ladoA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Y.ladoB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Y.ladoC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            areaX = X.Area();
+            areaY = Y.Area();
+
+            Console.WriteLine("AREA DO TRIAGULO X: " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("AREA DO TRIAGULO Y: " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Class05/Class05/Triangulo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Class05
+{
+    class Triangulo
+    {
+        public double ladoA, ladoB, ladoC;
+
+        public double Area() {
+            double p = (ladoA + ladoB + ladoC) / 2.0;
+            return Math.Sqrt(p * (p - ladoA) * (p - ladoB) * (p - ladoC));
+        }
+    }
+}
