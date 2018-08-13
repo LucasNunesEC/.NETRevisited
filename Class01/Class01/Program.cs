8740f65f38c55c00d0542e89426f5696aba1b039
Unknown <lucas.nunes@eng.ci.ufpb.br>
2018-08-13 09:12:35 -0300

    Entry/Exit Data

---
+++ Class01/Class01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Class01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            Console.WriteLine("Bem vindo, insira seu nome: ");
+            String nome = Console.ReadLine();
+            
+            Console.WriteLine("Insira sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Seu nome é: " + nome + " de idade " + idade + " anos");
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine(elapsedMs);
+            Console.ReadKey();
+
+        }
+    }
+}
